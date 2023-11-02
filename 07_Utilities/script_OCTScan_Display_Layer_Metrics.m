c8f26b0200ee664e938580208da2bb3270c44498
yzwang23 <149092764+yzwang23@users.noreply.github.com>
2023-11-01 21:37:29 -0500

    update

--- 07_Utilities/script_OCTScan_Display_Layer_Metrics.m
+++ 07_Utilities/script_OCTScan_Display_Layer_Metrics.m
@@ -30,8 +30,8 @@ scanPlotZLimit = 50;    % Z-axis limit in micrometer for 3-D layer map
 % post-processing to remove isolated EZ segments or not for DLM segment
 elimIsoEZSegment = 1;
 
-% which DL model's results to display
-kD = 1;                   % default -> first DLM
+% which segment in SegType to display
+segNum = 1;                 % which segment in SegType
 
 % plot parameters
 PlotParam = DefaultOCTVolScanPlotParam;
@@ -68,26 +68,58 @@ Im = BScanImage.ImageArray(:,:,1);      % get one-channel for gray scale
 
 % get segments
 Segments = HEOCTSortOCTScanSegmentation(BScanImage);
-if strcmpi(SegType, 'Auto') && isfield(Segments, 'Auto')
-  Segment = Segments.Auto(1).Segment;
-elseif strcmpi(SegType, 'Manu') && isfield(Segments, 'Manual')
-  Segment = Segments.Manual(1).Segment;
-elseif strcmpi(SegType, 'DMLM') && isfield(Segments, 'ML')
-  Segment = Segments.ML(1).SegmentData.Segment;
-elseif strcmpi(SegType, 'HDMLM') && isfield(Segments, 'ML')
-  Segment = Segments.ML(1).SegmentData_Hybrid.Segment;
-else
-  disp('no segmentation type defined. return ...');
-end
-
-% for DMLM & HDMLM, check to remove isolated EZ segments or not
-if strcmpi(SegType, 'DMLM') && elimIsoEZSegment~=0
-  OCTScan = UNetPPDeleteIsolatedLayerSegment(OCTScan, LN1, LN2, 'DMLM', kD);
-end
-
-% get 'HDMLM' hybrid deep machine learning model segmentation results
-if strcmpi(SegType, 'HDMLM') && elimIsoEZSegment~=0
-  OCTScan = UNetPPDeleteIsolatedLayerSegment(OCTScan, LN1, LN2, 'HDMLM', kD);
+switch SegType
+  case 'Auto'
+    if ~isempty(Segments.Auto)
+      kA = min(segNum, length(Segments.Auto));
+      AutoSegName = Segments.AutoSegNames{kA};
+      Segment = Segments.Auto(kA).Segment;
+      disp(['OS metrics measurements from ', AutoSegName, ' auto segmentation']);
+    else
+      disp('no HE auto segmentation. exit ...');
+      return;
+    end
+  case 'Manu'
+    if ~isempty(Segments.Manual)
+      kM = min(segNum, length(Segments.Manual));
+      ManuSegName = Segments.ManualGraders{kM};
+      Segment = Segments.Manual(kM).Segment;
+      disp(['OS metrics measurements from ', ManuSegName, ' manual segmentation']);
+    else
+      disp('no manual segmentation. exit ...');
+      return;
+    end
+  case 'DMLM'
+    if ~isempty(Segments.ML)
+      kD = min(segNum, length(Segments.ML));
+      DLMSegName = Segments.MLModelNames{kD};
+      Segment = Segments.ML(kD).SegmentData.Segment;
+      disp(['OS metrics measurements from ', DLMSegName, ' UNet segmentation']);
+      if elimIsoEZSegment~=0
+        % remove isolated EZ segments
+        OCTScan = UNetPPDeleteIsolatedLayerSegment(OCTScan, LN1, LN2, 'DMLM', kD);
+      end
+    else
+      disp('no UNet segmentation. exit ...');
+      return;
+    end
+  case 'HDMLM'
+    if ~isempty(Segments.ML) && isfield(Segments.ML, 'SegmentData_Hybrid')
+      kD = min(segNum, length(Segments.ML));
+      DLMSegName = Segments.MLModelNames{kD};
+      Segment = Segments.ML(kD).SegmentData_Hybrid.Segment;
+      disp(['OS metrics measurements from ', DLMSegName, ' hybrid model segmentation']);
+      if elimIsoEZSegment~=0
+        % remove isolated EZ segments
+        OCTScan = UNetPPDeleteIsolatedLayerSegment(OCTScan, LN1, LN2, 'HDMLM', kD);
+      end
+    else
+      disp('no hybrid model segmentation. exit ...');
+      return;
+    end
+  otherwise
+    disp('no segmentation type defined. exit ...');
+    return;
 end
 
 disp(['get thickness results of ', SegType, ' segmentation ...']);
